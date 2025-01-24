eaa6319786401f21b8dfba9f57573e0f6441436d
Dev-Dsouza <lourenco.h.colares@gmail.com>
2025-01-24 18:14:38 -0300

    backend added

---
+++ backend/TargetMasters/Models/BaseModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TargetMasters.Models
+{
+    public class BaseModel
+    {
+        public BaseModel()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.CreatedAt = DateTime.Now;
+            this.Deleted = false;
+        }
+
+
+        [Key]
+        [StringLength(36)]
+        public required string Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt {  get; set; }
+        public bool Deleted { get; set; }
+
+    }
+}
---
+++ backend/TargetMasters/Models/GuestName.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TargetMasters.Models
+{
+    public class GuestName : BaseModel
+    {
+        [Required]
+        [StringLength(20)]
+        public string Word { get; set; }
+        public WordType WordType { get; set; }
+    }
+
+    public enum WordType
+    {
+        Noun = 0,
+        Adjective,
+    }
+
+}
