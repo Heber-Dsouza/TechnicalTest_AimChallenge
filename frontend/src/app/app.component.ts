2a0adb626a99f6ac89867076983c22753d71d256
Dev-Dsouza <lourenco.h.colares@gmail.com>
2025-01-24 04:49:52 -0300

    styles and changes 0.10

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,107 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PlayerCardComponent } from '../components/game-page/player-card/player-card.component';
-import { GameTargetComponent } from '../components/game-page/game-target/game-target.component'; 
+import { GamePageComponent } from '../components/game-page/game-page.component'
 
-class CountdownTimer {
-  private intervalId: any = null;
-
-  constructor(public remainingTime: number, private onTick: (time: number) => void, private onFinish?: () => void) {}
-
-  start() {
-    this.intervalId = setInterval(() => {
-      this.remainingTime -= 1000;
-
-      if (this.remainingTime <= 0) {
-        this.stop();
-        this.onTick(0);
-        this.onFinish?.();
-      } else {
-        this.onTick(this.remainingTime);
-      }
-    }, 1000);
-  }
-
-  stop() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-      this.intervalId = null;
-    }
-  }
-}
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [PlayerCardComponent, CommonModule, GameTargetComponent],
+  imports: [CommonModule, GamePageComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'frontend';
-  random255() {
-    return Math.floor(Math.random() * 256);
-  }
-  players = [
-    {
-      playerName: 'Dsouza',
-      playerStats: {
-        secondsMs: 30000,
-        isActive: false,
-        randomColors: {
-          r: this.random255(),
-          g: this.random255(),
-          b: this.random255()
-        }
-      },
-    },
-  ];
-  target = {
-    showTarget: true,
-    targetSize: 30,
-    targetPosition: {
-      horizontal: {
-        name: "left",
-        value: 30
-      },
-      vertical: {
-        name: "top",
-        value: 30
-      },
-    }
-  };
-  getStyles(): { [key: string]: string } {
-    return {
-      width: (this.target.targetSize || 0) + 'px',
-      height: (this.target.targetSize || 0) + 'px',
-      [this.target.targetPosition.horizontal.name]: this.target.targetPosition.horizontal.value + '%',
-      [this.target.targetPosition.vertical.name]: this.target.targetPosition.vertical.value + '%'
-    };
-  }
-  targetStyleString = this.getStyles();
 
 
   constructor() {
-    this.initializeTimers();
   }
 
-  initializeTimers() {
-    const activePlayer = this.players.find(player => player.playerStats.isActive);
-  
-    if (activePlayer) {
-      const timer = new CountdownTimer(
-        activePlayer.playerStats.secondsMs,
-        (remainingTime) => {
-          activePlayer.playerStats.secondsMs = remainingTime;
-        },
-        () => {
-          console.log(`Timer do jogador ${activePlayer.playerName} finalizado.`);
-        }
-      );
-  
-      timer.start();
-    } else {
-      console.log('Nenhum jogador ativo encontrado.');
-    }
-  }
 }
---
+++ frontend/src/components/game-page/game-page.component.ts
@@ -0,0 +1,106 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PlayerCardComponent } from './player-card/player-card.component';
+import { GameTargetComponent } from './game-target/game-target.component'; 
+
+class CountdownTimer {
+  private intervalId: any = null;
+
+  constructor(public remainingTime: number, private onTick: (time: number) => void, private onFinish?: () => void) {}
+
+  start() {
+    this.intervalId = setInterval(() => {
+      this.remainingTime -= 1000;
+
+      if (this.remainingTime <= 0) {
+        this.stop();
+        this.onTick(0);
+        this.onFinish?.();
+      } else {
+        this.onTick(this.remainingTime);
+      }
+    }, 1000);
+  }
+
+  stop() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  }
+}
+
+@Component({
+  standalone: true,
+  selector: 'game-page',
+  imports: [CommonModule, PlayerCardComponent, GameTargetComponent],
+  templateUrl: './game-page.component.html',
+  styleUrls: ['./game-page.component.scss'],
+})
+export class GamePageComponent {
+  random255() {
+    return Math.floor(Math.random() * 256);
+  }
+  players = [
+    {
+      playerName: 'Dsouza',
+      playerStats: {
+        secondsMs: 30000,
+        isActive: false,
+        randomColors: {
+          r: this.random255(),
+          g: this.random255(),
+          b: this.random255()
+        }
+      },
+    },
+  ];
+  target = {
+    showTarget: true,
+    targetSize: 30,
+    targetPosition: {
+      horizontal: {
+        name: "left",
+        value: 30
+      },
+      vertical: {
+        name: "top",
+        value: 30
+      },
+    }
+  };
+  getStyles(): { [key: string]: string } {
+    return {
+      width: (this.target.targetSize || 0) + 'px',
+      height: (this.target.targetSize || 0) + 'px',
+      [this.target.targetPosition.horizontal.name]: this.target.targetPosition.horizontal.value + '%',
+      [this.target.targetPosition.vertical.name]: this.target.targetPosition.vertical.value + '%'
+    };
+  }
+  targetStyleString = this.getStyles();
+
+
+  constructor() {
+    this.initializeTimers();
+  }
+
+  initializeTimers() {
+    const activePlayer = this.players.find(player => player.playerStats.isActive);
+  
+    if (activePlayer) {
+      const timer = new CountdownTimer(
+        activePlayer.playerStats.secondsMs,
+        (remainingTime) => {
+          activePlayer.playerStats.secondsMs = remainingTime;
+        },
+        () => {
+          console.log(`Timer do jogador ${activePlayer.playerName} finalizado.`);
+        }
+      );
+  
+      timer.start();
+    } else {
+      console.log('Nenhum jogador ativo encontrado.');
+    }
+  }
+}
