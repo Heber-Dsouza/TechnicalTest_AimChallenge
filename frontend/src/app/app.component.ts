d2af85f5e517ae6bb998642ac3795add9cfc9e42
Dev-Dsouza <lourenco.h.colares@gmail.com>
2025-01-24 05:01:21 -0300

    styles and changes 0.11

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GamePageComponent } from '../components/game-page/game-page.component'
+import { HomePageConponent } from '../components/home-page/home-page.component'
 
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, GamePageComponent],
+  imports: [CommonModule, HomePageConponent, GamePageComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
